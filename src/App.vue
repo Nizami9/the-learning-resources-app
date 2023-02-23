45b2fdd73797866d5685735bd860b1b0daf06056
Nizami Suleymanov <nizami.suleymanoff@gmail.com>
2023-02-23 12:47:08 +0100

    adding dynamic component

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,12 @@
 <template>
   <the-header title="RememberMe"></the-header>
- <stored-resources :resources="storedResources"></stored-resources>
 </template>
 
 <script>
-  import StoredResources from "./components/learning-resources/StoredResources.vue"
   import TheHeader from "./components/layout/TheHeader.vue";
 export default {
   components: {
-   StoredResources, TheHeader
+  TheHeader
   },
   data() {
     return {
